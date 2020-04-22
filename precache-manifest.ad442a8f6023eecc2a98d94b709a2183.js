ea1efc2b44191f7589bfc266a07a50d6e13ba862
Adev22 <adevsaputra3@gmail.com>
2020-04-22 17:20:59 -0400

    Updates

---
+++ precache-manifest.ad442a8f6023eecc2a98d94b709a2183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4b3a61ee42fd375da610ddc667b5ab",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "864eff765cc005609607",
+    "url": "/recipe-finder/static/css/main.f0e0a517.chunk.css"
+  },
+  {
+    "revision": "d619aef54f2980604440",
+    "url": "/recipe-finder/static/js/2.ba31db16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-finder/static/js/2.ba31db16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "864eff765cc005609607",
+    "url": "/recipe-finder/static/js/main.3534e0a9.chunk.js"
+  },
+  {
+    "revision": "6a225ad17dc126917578",
+    "url": "/recipe-finder/static/js/runtime-main.d71c095d.js"
+  }
+]);
